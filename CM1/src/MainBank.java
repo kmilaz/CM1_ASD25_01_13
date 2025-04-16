51a793818ccd6eea3434c9ca863bbed07744e325
muiddd <muidabdulmuid99@gmail.com>
2025-04-16 09:53:53 +0700

    Commit arr bank

---
+++ CM1/src/MainBank.java
@@ -0,0 +1,11 @@
+public class MainBank {
+    public static void main(String[] args) {
+        Bank [] arrBank = new Bank[5];
+
+        arrBank[0] = new Bank("16030927", "Wallace", "Mei Mei","082-458-264-3263","wallace@gmail.com");
+        arrBank[1] = new Bank("16100617", "Darius", "Susanti","081-357-843-0547","darius@pt.otg");
+        arrBank[2] = new Bank("16240401", "Fuller", "Rosalia","085-556-712-7062","fuller@gmail.com");
+        arrBank[3] = new Bank("16270525", "Maria", "Krabela","082-267-223-0234","maria@gmail.com");
+        arrBank[4] = new Bank("16101617", "Gery", "Fatimah","083-683-416-8323","gery@gery.com");
+    }
+}
